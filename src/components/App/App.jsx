b283af8fbdb096a363513845d83847f01765a39a
Nadiia Khrystenko <nadiiakhrystenko1000@gmail.com>
2024-05-04 00:22:27 +0300

    New folder for App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
-import Profile from './Profile/Profile';
-import userData from '../userData.json';
+import Profile from '../Profile/Profile';
+import userData from '../../userData.json';
 
-import FriendList from './FriendList/FriendList';
-import friends from '../friends.json';
+import FriendList from '../FriendList/FriendList';
+import friends from '../../friends.json';
 
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import transactions from '../transactions.json';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import transactions from '../../transactions.json';
 
 export default function App() {
   return (
